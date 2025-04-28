b80ee3fc6c7e0a4872721cc0c03821caa9001737
Nick Yang <nick.yang@activenetwork.com>
2025-04-28 14:28:20 +0800

    Created page layout using tailwindcss

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Grid } from '@mui/material';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -10,8 +11,24 @@ import InboxIcon from '@mui/icons-material/Inbox';
 import DraftsIcon from '@mui/icons-material/Drafts';
 
 export default function TodolistPage() {
-return (<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f0f0f0', }}>
-            <Box sx={{ width: '100%', maxWidth: 960, bgcolor: 'background.paper', color: 'text.primary', borderRadius: '8px', boxShadow: 3 }}>
+return (<div id='backgroundContainer' className='flex w-screen h-screen bg-green-100'> 
+            <div id='contentContainer' className='w-4/5 bg-blue-100 mx-auto'>
+                <div id='header' className='flex w-auto bg-stone-300 gap-2 p-2 h-20'>
+                    <div className='w-1/3 md:w-1/6 bg-purple-300'>
+                        <p>To Dos</p>
+                    </div>
+                    <div className='w-1/3 md:w-1/6 bg-purple-300'>
+                        <p>Add</p>
+                    </div>
+                    <div className='w-1/3 md:w-1/6 bg-purple-300 ml-auto'>
+                        <p>Search</p>
+                    </div>
+                </div>
+                <div id='content' className='min-h-30 bg-amber-700'>
+                </div>
+            </div>
+        </div>);
+            {/* <Box sx={{ width: '100%', maxWidth: 960, bgcolor: 'background.paper', color: 'text.primary', borderRadius: '8px', boxShadow: 3 }}>
                 <nav aria-label="main mailbox folders">
                     <List>
                     <ListItem disablePadding>
@@ -47,6 +64,6 @@ return (<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'ce
                     </ListItem>
                     </List>
                 </nav>
-                </Box>
-        </div>);
+                </Box> */}
+        
 }
