fdef950e9a83fd1e93435585f8d40c9fdc8a9dc5
Nick Yang <nick.yang@activenetwork.com>
2025-04-29 13:59:48 +0800

    Customize theme to allow MUI components inherit the css set from tailwindcss

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,6 +1,6 @@
+"use client";
+
 import React from 'react';
-import { Grid } from '@mui/material';
-import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -9,61 +9,68 @@ import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import InboxIcon from '@mui/icons-material/Inbox';
 import DraftsIcon from '@mui/icons-material/Drafts';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    typography: {
+        //Inherite the font from the parent element set by tailwindcss  
+        fontFamily: 'inherit',
+    },
+});
 
 export default function TodolistPage() {
-return (<div id='backgroundContainer' className='flex w-screen h-screen bg-green-100'> 
-            <div id='contentContainer' className='w-4/5 bg-blue-100 mx-auto'>
-                <div id='header' className='flex w-auto bg-stone-300 gap-2 p-2 h-20'>
-                    <div className='w-1/3 md:w-1/6 bg-purple-300'>
-                        <p>To Dos</p>
+    return (
+        <ThemeProvider theme={theme}>
+            <div id='backgroundContainer' className='flex w-screen h-screen bg-[#f0f0f0] text-gray-800 font-sans'> 
+                <div id='contentContainer' className='w-4/5 bg-blue-300 mx-auto'>
+                    <div id='header' className='flex w-auto bg-stone-300 gap-2 p-2 h-20'>
+                        <div className='w-1/3 md:w-1/6 bg-purple-500'>
+                            <p>To Dos</p>
+                        </div>
+                        <div className='w-1/3 md:w-1/6 bg-purple-500'>
+                            <p>Add</p>
+                        </div>
+                        <div className='w-1/3 md:w-1/6 bg-purple-500 ml-auto'>
+                            <p>Search</p>
+                        </div>
                     </div>
-                    <div className='w-1/3 md:w-1/6 bg-purple-300'>
-                        <p>Add</p>
+                    <div id='content' className='min-h-30 bg-gray-50 p-2'>
+                        <List className='bg-cyan-300'>
+                            <ListItem disablePadding>
+                                <ListItemButton>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Inbox" />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem disablePadding>
+                                <ListItemButton>
+                                <ListItemIcon>
+                                    <DraftsIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Drafts" />
+                                </ListItemButton>
+                            </ListItem>
+                        </List>
+
+                        <Divider />
+
+                        <List className='bg-cyan-400'>
+                            <ListItem disablePadding>
+                                <ListItemButton>
+                                <ListItemText primary="Trash" />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem disablePadding>
+                                <ListItemButton component="a" href="#simple-list">
+                                <ListItemText primary="Spam" />
+                                </ListItemButton>
+                            </ListItem>
+                        </List>
                     </div>
-                    <div className='w-1/3 md:w-1/6 bg-purple-300 ml-auto'>
-                        <p>Search</p>
-                    </div>
-                </div>
-                <div id='content' className='min-h-30 bg-amber-700'>
                 </div>
             </div>
-        </div>);
-            {/* <Box sx={{ width: '100%', maxWidth: 960, bgcolor: 'background.paper', color: 'text.primary', borderRadius: '8px', boxShadow: 3 }}>
-                <nav aria-label="main mailbox folders">
-                    <List>
-                    <ListItem disablePadding>
-                        <ListItemButton>
-                        <ListItemIcon>
-                            <InboxIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Inbox" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton>
-                        <ListItemIcon>
-                            <DraftsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Drafts" />
-                        </ListItemButton>
-                    </ListItem>
-                    </List>
-                </nav>
-                <Divider />
-                <nav aria-label="secondary mailbox folders">
-                <List>
-                    <ListItem disablePadding>
-                        <ListItemButton>
-                        <ListItemText primary="Trash" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton component="a" href="#simple-list">
-                        <ListItemText primary="Spam" />
-                        </ListItemButton>
-                    </ListItem>
-                    </List>
-                </nav>
-                </Box> */}
-        
+        </ThemeProvider>
+    );        
 }
