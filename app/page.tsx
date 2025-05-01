944e7d8269668985556267156c8d38078e2bf1e6
Yang Yang <persistence@vip.qq.com>
2025-05-01 13:30:01 +0800

    complete a prototype ui of to-do list

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Button } from '@mui/material';
 import { YTextField } from '@/types/Components';
+import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
+import IncompleteCircleIcon from '@mui/icons-material/IncompleteCircle';
+import ChecklistIcon from '@mui/icons-material/Checklist';
 
 const theme = createTheme({
     typography: {
@@ -20,14 +20,36 @@ const theme = createTheme({
     },
 });
 
+const todolist = [
+    {
+        id: 1,
+        date: "2025-05-01",
+        summary: "To merge the code into the main branch",
+        status: 'completed',
+    },
+    {
+        id: 2,
+        date: "2025-05-03",
+        summary: "To complete add functionality to the page",
+        status: 'in-progress',
+    },
+    {
+        id: 3,
+        date: "2025-05-04",
+        summary: "To buy a new dumbbell",
+        status: 'not-started',
+    },
+];
+
 export default function TodolistPage() {
     const [value, setValue] = useState("");
     
     return (
         <ThemeProvider theme={theme}>
-            <div id='backgroundContainer' className='flex w-screen h-screen bg-[rgb(240,240,240)] text-gray-800 font-sans'> 
+            <div id='backgroundContainer' className='flex w-screen h-screen bg-[rgb(245,245,245)] text-gray-800 font-sans'> 
                 <div id='contentContainer' className='w-4/5 mx-auto'>
-                    <div id='header' className='flex w-auto h-20 p-4 items-end'>
+                    
+                    <div id='header' className='flex w-auto h-20 p-1 mb-5 items-end'>
                         <div className='flex items-center'>
                             <p className='text-3xl'>To-Dos</p>
                             <div className='ml-5'>
@@ -45,41 +67,31 @@ export default function TodolistPage() {
                         </div>
                     </div>
 
-                    <Divider />
-
-                    <div id='content' className='min-h-30 border-gray-300 border-[1] rounded-lg mt-5'>
-                        <List>
-                            <ListItem disablePadding>
-                                <ListItemButton>
-                                <ListItemIcon>
-                                    <InboxIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Inbox" />
-                                </ListItemButton>
+                    <div id='content' className='min-h-30 border-gray-300'>
+                        <List disablePadding className='bg-gray-50 border-[1px] rounded-lg border-gray-300'>
+                            <ListItem className='h-10 bg-gray-200 font-normal rounded-t-lg'>
+                                <p className='w-1/9'>Date</p>
+                                <p className='mr-5 w-1/3'>Summary</p>
+                                <p className='w-1/10'>Status</p>
                             </ListItem>
-                            <ListItem disablePadding>
-                                <ListItemButton>
-                                <ListItemIcon>
-                                    <DraftsIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Drafts" />
-                                </ListItemButton>
-                            </ListItem>
-                        </List>
-
-                        <Divider />
-
-                        <List>
-                            <ListItem disablePadding>
-                                <ListItemButton>
-                                <ListItemText primary="Trash" />
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem disablePadding>
-                                <ListItemButton component="a" href="#simple-list">
-                                <ListItemText primary="Spam" />
-                                </ListItemButton>
-                            </ListItem>
+                            {todolist.map((item) => (
+                                <div>
+                                    <Divider className='border-gray-300' />
+                                    <ListItem key={item.id} disablePadding className='font-extralight border-gray-300'>
+                                        <ListItemButton className='h-14'>
+                                            <p className='w-1/9'>{item.date}</p>
+                                            <p className='mr-5 w-1/3'>{item.summary}</p>
+                                            {/* <ListItemText className='w-1/10' primary={item.date} />
+                                            <ListItemText className='w-1/5'primary={item.summary} /> */}
+                                            <ListItemIcon className='w-1/10'>
+                                                {item.status === 'completed' && <CheckCircleOutlineRoundedIcon className="text-green-500" />}
+                                                {item.status === 'in-progress' && <IncompleteCircleIcon className="text-yellow-500" />}
+                                                {item.status === 'not-started' && <ChecklistIcon className="text-gray-500" />}
+                                            </ListItemIcon>
+                                        </ListItemButton>
+                                    </ListItem>
+                                </div>
+                            ))}
                         </List>
                     </div>
                 </div>
