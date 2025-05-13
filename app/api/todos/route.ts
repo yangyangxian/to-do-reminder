2c9fa8d4955b2b1015542cac5fffd288c45383a8
Nick Yang <nick.yang@activenetwork.com>
2025-05-13 14:08:53 +0800

    put query into data layer

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,10 +1,10 @@
-import { ExecuteSQL } from '@/dataAccess/dataAccess';
+import { ExecuteSQL, getAllTodos } from '@/dataAccess/dataAccess';
 import { FormatDate } from '@/utilities/formatHelper';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
     try {
-        var data = await ExecuteSQL('SELECT ut.id, ut.summary, ut.due_date, ut.status, ut.notes, tc.category_name FROM user_todos ut join todos_category tc on ut.category=tc.id order by ut.due_date asc;');
+        var data = await getAllTodos();
         const formattedData = data.map((item: any) => ({
             ...item,
             due_date: item.due_date ? FormatDate(item.due_date) : null,
